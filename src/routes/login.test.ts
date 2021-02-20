23f671507e7fe1c5a5a2bdef6ef6b52f8797aebd
fbosi <bosisoftwareengineering@protonmail.com>
2021-02-20 06:12:24 -0300

    Add login routes tests

---
+++ src/routes/login.test.ts
@@ -0,0 +1,72 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import app, { server } from '../app';
+import { connectToDataBase } from '../mongoConnection';
+
+describe('Test the login route', () => {
+  beforeAll(() => {
+    connectToDataBase();
+  });
+  afterAll((done) => {
+    server.close(done);
+    mongoose.disconnect(done);
+  });
+  test('It should receive a 200 for a fake user', (done) => {
+    request(app)
+      .post('/api/login')
+      .set('Accept', 'application/json')
+      .send({ userName: 'test', password: '123456' })
+      .then((res) => {
+        expect(res.status).toBe(200);
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('It should receive a 400 for a fake user with wrong password', (done) => {
+    request(app)
+      .post('/api/login')
+      .set('Accept', 'application/json')
+      .send({ userName: 'test', password: '12345' })
+      .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.message).toBe("Wrong password");
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('It should receive a 400 for a non-existing user', (done) => {
+    request(app)
+      .post('/api/login')
+      .set('Accept', 'application/json')
+      .send({ userName: 'nonExisting', password: '123456' })
+      .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.message).toBe("User not found");
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('It should receive a 400 for a call without password', (done) => {
+    request(app)
+      .post('/api/login')
+      .set('Accept', 'application/json')
+      .send({ userName: 'nonExisting'})
+      .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.message).toBe("Please write a password");
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,31 +1,31 @@
-import express from "express";
-import jwt from "jsonwebtoken";
+import express from 'express';
+import jwt from 'jsonwebtoken';
 
-import {notSoSecret} from '../app';
-import { decrypt } from "../util/encryption";
+import { notSoSecret } from '../app';
+import { decrypt } from '../util/encryption';
 
-import { User } from "../models/user";
+import { User } from '../models/user';
 
 export const loginRouter = express.Router();
 
-loginRouter.post("/api/login", async (req, res) => {
+loginRouter.post('/api/login', async (req, res) => {
   const { userName, password } = req.body;
 
   const user = await User.find({ userName });
 
-  if (!user) {
-    res.status(400).json("Usuário não encontrado");
+  if (!user[0]) {
+    res.status(400).json({ message: 'User not found' });
     return;
   }
   if (!password) {
-    res.status(400).json("Favor escrever sua senha");
+    res.status(400).json({ message: 'Please write a password' });
     return;
   }
 
   const senhaDoBancoDecrypt = decrypt(user[0].password);
 
   if (password !== senhaDoBancoDecrypt) {
-    res.status(400).json("Senha errada");
+    res.status(400).json({ message: 'Wrong password' });
     return;
   }
 
